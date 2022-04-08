28d07c54aa11e830238a05ea12484dabcfd560fc
longthuong <81215993+longthuong@users.noreply.github.com>
2022-04-08 20:27:40 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Python!"
+    return "This is Thuong's Python Lab!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
